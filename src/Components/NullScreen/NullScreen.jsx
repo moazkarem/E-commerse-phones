0691e701d25ccb846e27d6990cc38f20692f9b0f
moazkare7 <moazkare7@gmail.com>
2025-08-12 22:40:54 +0300

    enhance null screen design

--- src/Components/NullScreen/NullScreen.jsx
+++ src/Components/NullScreen/NullScreen.jsx
@@ -1,7 +1,16 @@
+import Lottie from "lottie-react";
 import React from "react";
+import nullanimation from "./../../Components/animation/emptyscreen.json";
 
 const NullScreen = ({ msg }) => {
-  return <div>{msg}</div>;
+  return (
+    <div className="flex justify-center items-center flex-col w-full h-screen gap-4">
+      <div>
+        <Lottie style={{ height: 380 }} animationData={nullanimation} />
+      </div>
+      <span className="text-[22px] text-[#fff]">{msg}</span>
+    </div>
+  );
 };
 
 export default NullScreen;
